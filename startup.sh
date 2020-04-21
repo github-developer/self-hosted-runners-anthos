185186cf098145b49ed79da13dd83ef9372408cd
bharathkkb <bharathkrishnakb@gmail.com>
2020-04-21 17:07:01 -0500

    lifecycle hook, runner as service
    docs: update README for Anthos usage
    
    fix: clean up workflow and use kustomize
    
    remove env check for now
    
    fix: docker auth
    
    fix: deploy
    
    CI: bump
    
    CI: bump
    
    chore: scale down replicas to 1
    
    CI: bump
    
    chore: add echo statements for debugging
    
    fix: ubuntu 18
    
    fix: let bin/installdependencies.sh take care of updates

--- startup.sh
+++ startup.sh
@@ -15,7 +15,7 @@ trap remove_runner EXIT
 CONFIG_TOKEN=$(curl --data "" --header "Authorization: Bearer $TOKEN" https://api.github.com/repos/$GITHUB_REPO/actions/runners/registration-token | jq -r '.token')
 
 # Create the runner and configure it
-./config.sh --url https://github.com/$GITHUB_REPO --token $CONFIG_TOKEN --unattended
+./config.sh --url https://github.com/$GITHUB_REPO --token $CONFIG_TOKEN --unattended --replace
 
 # Run it
 ./runsvc.sh
